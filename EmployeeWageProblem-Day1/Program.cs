cc7ec385563de2093c1b4f9123ac655cffdc4219
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-05 21:23:12 +0530

    Add: UC6-CalculateWagesTillCondition

--- EmployeeWageProblem-Day1/Program.cs
+++ EmployeeWageProblem-Day1/Program.cs
@@ -6,34 +6,35 @@
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int NUMBER_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-            Random random = new Random();
-            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
+                Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
-                    case 2:
+                    case IS_PART_TIME:
                         empHrs = 4;
                         break;
-                    case 1:
+                    case IS_FULL_TIME:
                         empHrs = 8;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
+                totalEmpHrs += empHrs;
             }
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Employee Wage is : " + totalEmpWage);
         }
-
     }
 }
 
